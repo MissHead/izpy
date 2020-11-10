8ad5c7951b72e9b6a92ef7032139ecfe95357578
Izabela <izabela.head@gmail.com>
2020-11-09 22:10:39 -0300

    refact version

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
 from setuptools import setup, find_packages
 
-if __name__ == "__main__":
-    setup(
-        name="izpy",
-        packages=find_packages(include=["izpy_errors", "izpy_requests"]),
-        version="0.0.1",
-        description="AbstractTest class for generic calls in integration testing of API's",
-        author="Izabela Ramos Ferreira",
-        install_requires=["requirements"]
-    )
+version = "1.0.0"
+
+# python setup.py tag
+if sys.argv[-1] == 'tag':
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+# python setup.py publish
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel upload")
+    sys.exit()
+
+setup(name="izpy",
+      version=version,
+      description="AbstractTest class for generic calls in integration testing of API's",
+      license="GNU GENERAL PUBLIC LICENSE",
+      install_requires=["json","requests"],
+      author="Izabela Ramos Ferreira",
+      author_email="izabela.head@gmail.com",
+      url="https://github.com/MissHead/izpy",
+      packages = find_packages(),
+      keywords= "generic calls, curl",
+      zip_safe = True)
